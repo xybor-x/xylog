d166346e1f10a81b08569baa00a2536c54cb228a
huykingsofm <huykingsofm@gmail.com>
2022-09-16 17:23:11 +0000

    Add copyright to all files (#33)

--- encoding/encoder.go
+++ encoding/encoder.go
@@ -1,7 +1,30 @@
+// MIT License
+//
+// Copyright (c) 2022 xybor-x
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package encoding
 
 import "fmt"
 
+// Encoding instances allow to encode with a specified format.
 type Encoding interface {
 	// addString adds a field of string to the Encoder.
 	addString(k, v string)
@@ -31,14 +54,17 @@ type Encoding interface {
 	free()
 }
 
+// Encoder is a wrapper struct of Encoding.
 type Encoder struct {
 	encoding Encoding
 }
 
+// NewEncoder returns a Encoder with a specified Encoding.
 func NewEncoder(e Encoding) *Encoder {
 	return &Encoder{encoding: e}
 }
 
+// Add is a generic method using to encode a generic value.
 func (encoder *Encoder) Add(k string, v any) {
 	switch t := v.(type) {
 	case string:
